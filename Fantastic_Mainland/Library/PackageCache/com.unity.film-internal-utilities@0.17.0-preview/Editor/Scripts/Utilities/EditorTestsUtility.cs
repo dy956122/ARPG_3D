fff800bb6741032c3cf6ccf7613546573cb139ff
LinChungWen <dy956122@gmail.com>
2023-07-13 14:51:23 +0800

    [Chore] UTS 0.85 與 Film Internal Utilities 0.17.0 版 為 綁定版本
    UTS 升到 0.9 版以上會發生問題

---
+++ Fantastic_Mainland/Library/PackageCache/com.unity.film-internal-utilities@0.17.0-preview/Editor/Scripts/Utilities/EditorTestsUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace Unity.FilmInternalUtilities.Editor {
+
+internal static class EditorTestsUtility {
+                
+    [Obsolete("Replaced by YieldEditorUtility.WaitForFramesAndIncrementUndo()")]
+    internal static IEnumerator WaitForFrames(int numFrames) {
+        yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(numFrames);
+    }
+    
+}
+
+} //end namespace
---
+++ Fantastic_Mainland/Library/PackageCache/com.unity.film-internal-utilities@0.17.0-preview/Runtime/Scripts/Utilities/ObjectUtility.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Object = UnityEngine.Object;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Unity.FilmInternalUtilities {
+internal static class ObjectUtility {
+
+#if UNITY_2020_3_OR_NEWER
+
+    internal static IEnumerable<T> FindSceneComponents<T>(bool includeInactive = true) where T: UnityEngine.Component {
+        foreach (T comp in Object.FindObjectsOfType<T>(includeInactive)) {
+            yield return comp;
+        }
+    }
+#else
+    internal static IEnumerable<T> FindSceneComponents<T>() where T: UnityEngine.Component {
+        foreach (Object o in Resources.FindObjectsOfTypeAll(typeof(T))) {
+            T comp = (T) o;
+            Assert.IsNotNull(comp);
+            GameObject go = comp.gameObject;
+
+            if (!(go.hideFlags == HideFlags.NotEditable || go.hideFlags == HideFlags.HideAndDontSave)
+#if UNITY_EDITOR                    
+                && !EditorUtility.IsPersistent(comp.transform.root.gameObject)
+#endif                    
+               ) 
+            {
+                yield return comp;
+                    
+            }
+        }
+    }
+    
+#endif
+
+
+
+//----------------------------------------------------------------------------------------------------------------------       
+
+    internal static T[] ConvertArray<T>(Object[] objs) where T :  UnityEngine.Object{
+        int numObjects = objs.Length;
+        T[] ret = new T[numObjects];
+        for (int i = 0; i < numObjects; i++) {
+            ret[i] = objs[i] as T;
+        }
+        return ret;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------       
+    
+    internal static void Destroy(Object obj, bool forceImmediate = false) {
+
+        //Handle differences between editor/runtime when destroying immediately
+#if UNITY_EDITOR
+        if (!Application.isPlaying || forceImmediate) {
+            Undo.DestroyObjectImmediate(obj);
+        }
+#else
+        if (forceImmediate) {
+            Object.DestroyImmediate(obj);
+        }
+#endif
+        else {
+            Object.Destroy(obj);
+        }
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------       
+    /// <summary>
+    /// Create a GameObject with a Component
+    /// </summary>
+    /// <param name="goName">The name of the GameObject</param>
+    /// <typeparam name="T">The type of the Component</typeparam>
+    /// <returns>The newly created GameObject</returns>
+    [Obsolete] 
+    internal static T CreateGameObjectWithComponent<T>(string goName) where T: Component {
+        GameObject go        = new GameObject(goName);
+        T          component = go.AddComponent<T>();
+        return component;        
+    }
+}
+
+} //end namespace
