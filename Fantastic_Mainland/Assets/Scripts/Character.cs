6509054b3690b1dc529b30806c8e41fd5659ec37
LinChungWen <dy956122@gmail.com>
2023-07-31 17:37:21 +0800

    [Feat] 追加 角色 的基礎屬性、行為

---
+++ Fantastic_Mainland/Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+public class Character : CharacterProperty
+{
+    protected override void Attack()
+    {
+        print("Attack");
+    }
+
+    protected override void Walk()
+    {
+        print("walk");
+    }
+
+    void Awake()
+    {
+        Attack();
+        Walk();
+    }
+
+    void Start()
+    {
+        hp = 15;
+        mp = 30;
+        atk = 10;
+        def = 16;
+        speed = 23;
+    }
+}
---
+++ Fantastic_Mainland/Assets/Scripts/CharacterProperty.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+public abstract class CharacterProperty : MonoBehaviour
+{
+    [SerializeField] protected int hp, mp, atk, def, speed;
+    protected virtual void Attack() { }
+    protected virtual void Walk() { }
+
+    // 給系統通知用的
+    protected event Action Death;
+}
