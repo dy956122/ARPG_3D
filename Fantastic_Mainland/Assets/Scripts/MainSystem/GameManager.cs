3232b5a60793368b4da3e9d464f6a85a767623c2
LinChungWen <dy956122@gmail.com>
2023-08-07 13:46:02 +0800

    [Feat] 追加其他新的 Manager Code
    目前增加:
    1. Asset
    2. Database
    3. Level

---
+++ Fantastic_Mainland/Assets/Scripts/MainSystem/GameManager.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public LevelManager levelMgr;
+    public AssetManager assetMgr;
+    public DatabaseManager databaseMgr;
+
+
+
+
+
+}
